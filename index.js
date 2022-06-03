8cd7d2a36f819002875b1997477ad68b8afea7ce
sm-jahangir <samraatjahangir@gmail.com>
2022-06-03 15:46:25 +0600

    created user api

--- index.js
+++ index.js
@@ -14,35 +14,25 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    console.log("decoded", decoded);
-    req.decoded = decoded;
-    next();
-  });
-}
+
 async function run() {
   try {
     await client.connect();
     const productCollection = client.db("jahanParts").collection("products");
     const orderCollection = client.db("jahanParts").collection("orders");
     const reviewCollection = client.db("jahanParts").collection("reviews");
+    const userCollection = client.db("jahanParts").collection("users");
 
-    // AUTH
-    app.post("/login", async (req, res) => {
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
       const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
     });
 
     // Get all services
