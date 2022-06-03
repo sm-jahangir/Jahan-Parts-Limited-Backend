01e3a426b4698a4f09cad80022211f02de8c9b57
sm-jahangir <samraatjahangir@gmail.com>
2022-06-03 15:55:14 +0600

    verify jwt token

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -32,7 +47,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
 
     // Get all services
@@ -76,7 +96,7 @@ async function run() {
       const email = req.query.email;
       if (email === decodedEmail) {
         const query = { email: email };
-        const cursor = orderCollection.find(query);
+        const cursor = reviewCollection.find(query);
         const reviews = await cursor.toArray();
         res.send(reviews);
       } else {
